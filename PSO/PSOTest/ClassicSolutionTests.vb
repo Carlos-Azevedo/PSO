d9c85032255da0474cb31420cf24835c70c411f9
Carlos.Azevedo <CarlosAzevedo.git@gmail.com>
2015-07-18 08:25:40 -0300

    Removed incorrect Tests Project

--- PSO/PSOTest/ClassicSolutionTests.vb
+++ PSO/PSOTest/ClassicSolutionTests.vb
@@ -1,7 +1,7 @@
 ﻿Imports System.Text
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
-<TestClass()> Public Class UnitTest1
+<TestClass()> Public Class ClassicSolutionTests
 
     <TestMethod()> Public Sub TestMethod1()
     End Sub
